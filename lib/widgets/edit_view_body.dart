1cedbfc445a2dc0cb57045eae9c6788359489078
nobil22 <mohamednabilmn833592@gmail.com>
2024-12-14 13:46:41 +0200

    add edit notes view

--- lib/widgets/edit_view_body.dart
+++ lib/widgets/edit_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/widgets/custom_appbar.dart';
+import 'package:note_app/widgets/custom_text_field.dart';
 
 class EditViewBody extends StatelessWidget {
   const EditViewBody({super.key});
@@ -11,7 +12,11 @@ class EditViewBody extends StatelessWidget {
       child: Column(
         children: [
           SizedBox(height:25 ,),
-          CustomAppbar(title: 'Edit Note', icon: Icons.check)
+          CustomAppbar(title: 'Edit Note', icon: Icons.check),
+           SizedBox(height:50),
+           CustomTextField(hint: 'title'),
+            SizedBox(height:20),
+             CustomTextField(hint: 'content',maxline: 5,),
         ],
       ),
     );
